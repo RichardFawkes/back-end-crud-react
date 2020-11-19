d8d913c11d04c6f1735958ef6b9f4677f1834901
Richard <richardhackers@gmail.com>
2020-11-19 10:28:34 -0300

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,20 @@
+//STRING DE CONEXAO
+
+module.exports = {
+    development: {
+        database: {
+            host: 'dbsreact.ci3m3x5uzji8.us-east-2.rds.amazonaws.com',
+            port: 3306,
+            name: 'dbreact',
+            dialect: 'mysql',
+            user: 'admin',
+            password: '26040604'
+        }
+    },
+    production:{
+        database: {
+            host: process.env.DB_HOST,
+            host: process.env.DB_PORT
+        }
+    }
+}
